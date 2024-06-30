74ac369d023b0d8e08261cbeb4921f30eafc5ff4
Carlos Júnior <jrbrandao1@live.com>
2024-06-30 16:17:54 -0300

    Atualização 1.0

---
+++ script.js
@@ -0,0 +1,32 @@
+async function obterEndereco(cep) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    return data;
+}
+
+async function obterPrevisao(latitude, longitude) {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    const data = await response.json();
+    return data;
+}
+
+async function buscarCep() {
+    const cep = document.getElementById('cep').value;
+    const resultado = await obterEndereco(cep);
+    const tr = document.getElementById('endereco');
+    tr.innerHtml = `<td>${resultado.logradouro}</td>
+    <td>${resultado.bairro}</td> <td>${resultado.uf}</td>`;
+
+}
+
+
+async function buscarTemperatura() {
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const resultado = await obterPrevisao(latitude, longitude);
+    const temperatura = resultado.hourly.temperature_2m[0]
+    const span = document.getElementById('temperatura')
+    span.innerHTML = temperatura;
+}
+
+
